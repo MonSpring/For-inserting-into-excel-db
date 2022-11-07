f8d0131722cada594182ca480449abc7692acd51
이창욱 <90178632+leechangwook92@users.noreply.github.com>
2022-11-07 15:28:40 +0900

    Delete BookRepository.java

--- src/main/java/com/example/poiprj/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.poiprj.repository;
-
-import com.example.poiprj.domain.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-    //Books findByBooks(Books books);
-}
